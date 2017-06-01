84352d30c5cfe045e3d0b218d39bee1e9a52cf4a
erdenbatuhan <erden.batuhan@gmail.com>
2017-06-01 14:14:56 +0300

    Updated iOS

---
+++ PiRoom/Model/BuildingDataSource.swift
@@ -0,0 +1,35 @@
+//
+//  BuildingDataSource.swift
+//  PiRoom
+//
+//  Copyright © 2017 O.B.E. Software & Development. All rights reserved.
+//
+
+import Foundation
+
+class BuildingDataSource: NSObject {
+    
+    var buildingArray : [Building] = []
+    var building1: Building
+    var building2: Building
+    var building3: Building
+    var building4: Building
+    var building5: Building
+    
+    override init ()
+    {
+        
+        self.building1 = Building(imageName: "ab1", name: "Engineering Building", capacity: 0, fullRoomNumber: 0)
+        self.building2 = Building(imageName: "ab2", name: "Administration Building", capacity: 0, fullRoomNumber: 0)
+        self.building3 = Building(imageName: "ab3", name: "Law Building", capacity: 0, fullRoomNumber: 0)
+        self.building4 = Building(imageName: "ab4", name: "Scola Building", capacity: 0, fullRoomNumber: 0)
+        self.building5 = Building(imageName:"ab5",name: "Innovation Center", capacity: 0, fullRoomNumber: 0)
+        
+        buildingArray.append(building1)
+        buildingArray.append(building2)
+        buildingArray.append(building3)
+        buildingArray.append(building4)
+        buildingArray.append(building5)
+        
+    }
+}
---
+++ PiRoom/View/RoomCellView.swift
@@ -0,0 +1,32 @@
+//
+//  RoomCellView.swift
+//  PiRoom
+//
+//  Copyright © 2017 O.B.E. Software & Development. All rights reserved.
+//
+
+import UIKit
+
+class RoomCellView: UIView {
+
+    let SHADOW_GREY: CGFloat = 120.0 / 255.0
+    
+    override func awakeFromNib() {
+        
+        super.awakeFromNib()
+        
+        layer.cornerRadius = 10.0
+        
+        layer.shadowColor = UIColor(red: SHADOW_GREY , green: SHADOW_GREY, blue: SHADOW_GREY, alpha: 0.6).cgColor
+        layer.shadowOpacity = 1.0
+        layer.shadowRadius = 5.0
+        layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
+    }
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        layer.cornerRadius = 10.0
+        
+    }
+
+}
